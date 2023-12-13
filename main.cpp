83bbe14528e54532b5f05494a6ed2faadca0dd53
dev-pa5an <pasanfernando24@gmail.com>
2023-12-13 22:39:40 +0530

    Initial Menu

--- main.cpp
+++ main.cpp
@@ -2,6 +2,18 @@
 
 int main(){
     
-    std::cout << "Hello Pasan, welome to C++" << std::endl;
+    // 1 print help
+    std::cout << "1: Print help" << std::endl;
+    // 2 print exchange stats
+    std::cout << "2: Print exchange stats" << std::endl;
+    // 3 make an offer
+    std::cout << "3: Make an offer" << std::endl;
+    // 4 make a bid 
+    std::cout << "4: Make a bid" << std::endl;
+    // 5 print wallet
+    std::cout << "5: Print wallet" << std::endl;
+    // 6 continue
+    std::cout << "6: Continue" << std::endl;
+
     return 0;
 }
