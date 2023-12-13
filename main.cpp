1554e45ff60e10937db2ce856c64979472376576
dev-pa5an <pasanfernando24@gmail.com>
2023-12-14 00:45:04 +0530

    Application main menu

--- main.cpp
+++ main.cpp
@@ -10,20 +10,43 @@ int main(){
         // 2 print exchange stats
         std::cout << "2: Print exchange stats" << std::endl;
         // 3 make an offer
-        std::cout << "3: Make an offer" << std::endl;
+        std::cout << "3: Place an ask" << std::endl;
         // 4 make a bid 
-        std::cout << "4: Make a bid" << std::endl;
+        std::cout << "4: Place a bid" << std::endl;
         // 5 print wallet
         std::cout << "5: Print wallet" << std::endl;
         // 6 continue
         std::cout << "6: Continue" << std::endl;
 
         std::cout << "========================" << std::endl;
-        std::cout << "Select from 1-6" << std::endl;
+        std::cout << "Type in 1-6" << std::endl;
         
         int userOption;
         std::cin >> userOption;
         std::cout << "You select " << userOption << std::endl;
+
+        if (userOption == 1){
+            std::cout << "Help - choose options from the menu" << std::endl;
+            std::cout << "and follow the on screen instructions." << std::endl;
+        }
+        if (userOption == 2){
+            std::cout << "Exchange Stats: View statistics related to the exchange." << std::endl;
+        }
+        if (userOption == 3){
+            std::cout << "Place an Ask: Enter details to make an ask on the exchange." << std::endl;
+        }
+        if (userOption == 4){
+            std::cout << "Place a Bid: Enter details to make a bid on the exchange." << std::endl;
+        }
+        if (userOption == 5){
+            std::cout << "Print Wallet: Display information about your wallet." << std::endl;
+        }
+        if (userOption == 6){
+            std::cout << "Continue: Proceed to the next step." << std::endl;
+        }
+        if (userOption < 1 ||  userOption > 6){
+            std::cout << "Invalid Option - Please choose a number between 1-6" << std::endl;
+        }
     }
 
     return 0;
