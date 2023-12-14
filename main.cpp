b1f05a4d7dd7a92a04d10e8d4730d853fc281710
dev-pa5an <pasanfernando24@gmail.com>
2023-12-14 21:04:49 +0530

    Refactoring the code

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 
-void printMenu(){
-
+void printMenu()
+{
      // 1 print help
         std::cout << "1: Print help" << std::endl;
         // 2 print exchange stats
@@ -18,50 +18,82 @@ void printMenu(){
         std::cout << "========================" << std::endl;
 }
 
-int getuserOption(){
-
-    std::cout << "Type in 1-6" << std::endl;
-        
+int getuserOption()
+{
+    std::cout << "Type in 1-6" << std::endl;   
     int userOption;
     std::cin >> userOption;
     std::cout << "You select " << userOption << std::endl;
 
     return userOption;
 }
+void printHelp()
+{
+    std::cout << "Help - choose options from the menu" << std::endl;
+    std::cout << "and follow the on screen instructions." << std::endl;
+}
+void printMarketStats()
+{
+    std::cout << "Exchange Stats: View statistics related to the exchange." << std::endl;
+}
+void enterOffer()
+{
+    std::cout << "Place an Ask: Enter details to make an ask on the exchange." << std::endl;
+}
+void enterBid()
+{
+    std::cout << "Place a Bid: Enter details to make a bid on the exchange." << std::endl;
 
+}
+void printWallet()
+{
+    std::cout << "Print Wallet: Display information about your wallet." << std::endl;
+
+}
+void goToNextWallet()
+{
+    std::cout << "Continue: Proceed to the next step." << std::endl;
+
+}
 void processUserOption(int userOption){
 
-    if (userOption == 1){
-            std::cout << "Help - choose options from the menu" << std::endl;
-            std::cout << "and follow the on screen instructions." << std::endl;
-        }
-        if (userOption == 2){
-            std::cout << "Exchange Stats: View statistics related to the exchange." << std::endl;
-        }
-        if (userOption == 3){
-            std::cout << "Place an Ask: Enter details to make an ask on the exchange." << std::endl;
-        }
-        if (userOption == 4){
-            std::cout << "Place a Bid: Enter details to make a bid on the exchange." << std::endl;
-        }
-        if (userOption == 5){
-            std::cout << "Print Wallet: Display information about your wallet." << std::endl;
-        }
-        if (userOption == 6){
-            std::cout << "Continue: Proceed to the next step." << std::endl;
-        }
-        if (userOption < 1 ||  userOption > 6){
-            std::cout << "Invalid Option - Please choose a number between 1-6" << std::endl;
-        }
+    if (userOption == 1)
+    {
+        printHelp();
+    }
+    if (userOption == 2)
+    {
+        printMarketStats();
+    }
+    if (userOption == 3)
+    {
+        enterOffer();
+    }
+    if (userOption == 4)
+    {
+        enterBid();
+    }
+    if (userOption == 5)
+    {
+        printWallet();
+    }
+    if (userOption == 6)
+    {
+        goToNextWallet();
+    }
+    if (userOption < 1 ||  userOption > 6)
+    {
+        std::cout << "Invalid Option - Please choose a number between 1-6" << std::endl;
+    }
 }
 
 int main(){
     
-    while(true){
+    while(true)
+    {
         printMenu();
         int userOption = getuserOption();
-        processUserOption(userOption);
-        
+        processUserOption(userOption);  
     }
 
     return 0;
