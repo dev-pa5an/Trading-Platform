140d08be6b5fcecba70f40b37d27eed0db784b15
dev-pa5an <pasanfernando24@gmail.com>
2023-12-25 10:22:21 +0530

    Exception handling of csv file

--- test.cpp
+++ test.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <fstream>
 
 std::vector<std::string> tokenise(std::string csvLine, char seperator)
 {
@@ -28,14 +29,52 @@ std::vector<std::string> tokenise(std::string csvLine, char seperator)
 
 int main()
 {
-    std::string s = "thing,thing1,thing2";
-    std::vector<std::string> tokens;
+    // std::string s = "thing,thing1,thing2";
+    // std::vector<std::string> tokens;
 
-    tokens = tokenise(s, ',');
+    // tokens = tokenise(s, ',');
 
-    for (std::string& t : tokens)
+    // for (std::string& t : tokens)
+    // {
+    //     std::cout << t << std::endl;
+    // }
+
+    std::ifstream csvFile{"20231224.csv"};
+    std::string line;
+
+    if (csvFile.is_open())
     {
-        std::cout << t << std::endl;
+        std::cout << "File open" << std::endl;
+        std::vector<std::string> tokens;
+
+        while (std::getline(csvFile, line))
+        {
+            std::cout << line << std::endl;
+            tokens = tokenise(line, ',');
+
+            if (tokens.size() != 5)
+            {
+                std::cout << "Bad line" << std::endl;
+                continue;
+            }
+            //we have 5 tokens
+            try
+            {
+                double price = std::stod(tokens[3]);
+                double amount = std::stod(tokens[4]);
+            }
+            catch(const std::exception& e)
+            {
+                std::cerr << e.what() << '\n';
+                continue;
+            }
+        }
+        csvFile.close();
+        
+    }
+    else
+    {
+        std::cout << "Could not open" << std::endl;
     }
     
 
