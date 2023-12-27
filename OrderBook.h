eb801b7a7964dcf366912bcfe53f57300573c534
dev-pa5an <pasanfernando24@gmail.com>
2023-12-27 10:09:04 +0530

    Seperating orders by type, product and time

---
+++ OrderBook.h
@@ -0,0 +1,14 @@
+#pragma once
+#include "OrderBookEntry.h"
+#include "CSVReader.h"
+#include <string>
+
+class OrderBook
+{
+    public:
+        OrderBook(std::string filename);
+
+        std::vector<std::string> getKnownProducts();
+
+        std::vector<OrderBookEntry> getOrders(OrderBookType type, std::string product, std::string timestamp);
+};
